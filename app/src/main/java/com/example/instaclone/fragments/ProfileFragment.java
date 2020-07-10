22601b2de3e844ef71094a072fd24974a4bb82b7
azhareus <azhar.hj@gmail.com>
2020-07-10 01:42:19 -0400

    Added a logout feature and created a new layout for user

--- app/src/main/java/com/example/instaclone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instaclone/fragments/ProfileFragment.java
@@ -1,21 +1,89 @@
 package com.example.instaclone.fragments;
 
-import android.util.Log;
+import android.content.Intent;
+import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.instaclone.LoginActivity;
 import com.example.instaclone.Post;
+import com.example.instaclone.PostsAdapter;
+import com.example.instaclone.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProfileFragment extends PostsFragment {
+public class ProfileFragment extends Fragment {
+
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List <Post> allPosts;
+    public int totalPosts = 20;
+    private Button btnLogout;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.putExtra("logout", currentUser);
+                startActivity(intent);
+            }
+        });
+
+
+        //Steps to use the recycler view:
+        //0. create layout for one row in the list
+        //1. create the apapter
+        //2. create the data source
+        //3. set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        //4. set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+    }
     protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
         query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
-        query.setLimit(20);
+
+        query.setLimit(totalPosts);
         query.addDescendingOrder(Post.KEY_CREATED_KEY);
         query.findInBackground(new FindCallback<Post>() {
             @Override
@@ -32,5 +100,4 @@ public class ProfileFragment extends PostsFragment {
             }
         });
     }
-
 }
